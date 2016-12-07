38e01611a30aa17a52a7420eb1c2a46da97c0970
ShadiJam <shadijam00@gmail.com>
2016-12-07 07:52:48 -0600

    edits

--- calc.js
+++ calc.js
@@ -1,24 +1,47 @@
 
 
+
+
 const app = () => {
     const React = react,
-        DOM = reactDom
+        DOM = reactDom    
     
     class Calculator extends React.Component {
-        handle(e, i){
+        constructor(p){
+            super(p)
+            this.state = {}
+        }
+        eval(x) {
+            const result = this.state.result || '';
+            
+            switch (x) {
+                case 'C': return ``;
+                case 'x': return `${result}*`;
+                case '=': return eval(result);
+                default: return `${result}${x}`;
+            }
+        }
+        handle(e, x){
             e.preventDefault()
-            log(`${i} was clicked!`)
+            this.setState({ result: this.eval(x) })
         }
         render(){
-            return <div>
-                {new Array(10).fill(1).map((x,i) =>
-                    <div 
-                        onClick={e => this.handle(e,i)}>
-                        {i}
-                    </div>
-                )}
+            return (
+                <div className="calculator">
+                <div className="a1-4" onClick={e => this.handle(e, 'C')}>C</div>
+                <div className="a3-4">{this.state.result}</div>
+                {
+                    [7,8,9,'/',4,5,6,'x',1,2,3,'-',0,'.','=','+']
+                    	.map(x => 
+                             <div
+                             	className="a1-4"
+                             	onClick={e => this.handle(e, x)}>
+                             	{x}
+                             </div>
+                        )
+                }
             </div>
-        }
+            )}
     }
     
     DOM.render(<Calculator />, document.body)
